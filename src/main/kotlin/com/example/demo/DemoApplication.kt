f2d760d37808733965bf42d408d29e1645c49032
ryo-tateishi <ryo-tateishi@bm-sms.co.jp>
2020-04-03 18:43:47 +0900

    Add sample api

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,10 +2,18 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class SampleAPI {
+    @GetMapping("sample")
+    fun sample(): String = "Hello World!"
 }
